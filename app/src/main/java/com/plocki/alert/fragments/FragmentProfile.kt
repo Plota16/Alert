df00aef4171bbcaff87ac5b4c70bb0ce815d49c2
maciejKrasny <maciej.krasny97@gmail.com>
2020-01-07 10:51:48 +0100

    Get user stats in onCreate profile fragment

--- app/src/main/java/com/plocki/alert/fragments/FragmentProfile.kt
+++ app/src/main/java/com/plocki/alert/fragments/FragmentProfile.kt
@@ -6,8 +6,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.apollographql.apollo.ApolloCall
+import com.apollographql.apollo.api.Response
+import com.apollographql.apollo.exception.ApolloException
+import com.google.gson.GsonBuilder
+import com.plocki.alert.API.modules.ReportCategoriesApi
+import com.plocki.alert.API.modules.StatsApi
+import com.plocki.alert.AllReportCategoriesQuery
+import com.plocki.alert.MyStatsQuery
 import com.plocki.alert.R
+import com.plocki.alert.models.Global
 import com.plocki.alert.utils.AppLauncher
+import com.plocki.alert.utils.HttpErrorHandler
 import kotlinx.android.synthetic.main.fragment_profile.*
 
 class FragmentProfile : Fragment() {
@@ -20,7 +30,26 @@ class FragmentProfile : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        if(Global.getInstance()!!.userToken != "") {
+            println(Global.getInstance()!!.userToken)
+            StatsApi.fetchStats(object : ApolloCall.Callback<MyStatsQuery.Data>(){
+                override fun onResponse(response: Response<MyStatsQuery.Data>) {
+                    if (response.hasErrors()) {
+                        val gson = GsonBuilder().create()
+                        val errorMap = gson.fromJson(response.errors()[0].message(), Map::class.java)
+                        HttpErrorHandler.handle(errorMap["statusCode"].toString().toFloat().toInt())
+                        return
+                    }
+                    println("PROFILE FRAAGMENT STATS " + response.data().toString())
+                }
+                override fun onFailure(e: ApolloException) {
 
+                    HttpErrorHandler.handle(500)
+                }
+
+            })
+
+        }
         siqn_out_button.setOnClickListener{
             AppLauncher.logOut()
         }
