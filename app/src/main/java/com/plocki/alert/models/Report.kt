d7084f13f48e0f7b855f355cdd7696d33bc1727b
maciejKrasny <maciej.krasny97@gmail.com>
2020-01-07 12:47:05 +0100

    Add event to report

--- app/src/main/java/com/plocki/alert/models/Report.kt
+++ app/src/main/java/com/plocki/alert/models/Report.kt
@@ -5,13 +5,15 @@ import com.plocki.alert.type.CreateReportDto
 
 class Report(
     var category: Category,
-    var description: String
+    var description: String,
+    var event: Event
 ) {
 
     fun createReportDto(context: Context?): CreateReportDto {
         val reportBuilder = CreateReportDto.builder()
             .description(this.description)
             .category(this.category.uuid)
+            .event(this.event.UUID.toString())
 
         return reportBuilder.build()
     }
